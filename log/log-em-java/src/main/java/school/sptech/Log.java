c5011ad7228357294e3efa3d528ceb09e9a621ad
JosueAlvarezAvendano <josue.avendano@sptech.school>
2026-05-01 18:16:51 -0300

    Log conectado com o banco de dados

--- log/log-em-java/src/main/java/school/sptech/Log.java
+++ log/log-em-java/src/main/java/school/sptech/Log.java
@@ -5,21 +5,73 @@ import java.time.format.DateTimeFormatter;
 
 public class Log {
 
-    String mensagem;
-    String tipo;
-    LocalDateTime dataHora;
+    private Integer idLog;
+    private String mensagem;
+    private String tipo;
+    private LocalDateTime dataHora;
+    private Integer fkUsuario;
 
-    public void preencherLog(String mensagemRecebida, String tipoRecebido) {
-        mensagem = mensagemRecebida;
-        tipo = tipoRecebido;
-        dataHora = LocalDateTime.now();
+    // Construtor SEM usuário (evento de sistema)
+    public Log(String mensagem, String tipo) {
+        this.mensagem = mensagem;
+        this.tipo = tipo;
+        this.dataHora = LocalDateTime.now();
+        this.fkUsuario = null;
     }
 
-    // Transforma o Log em String e retorna para a exibição
-    public String formatarLog() {
-        DateTimeFormatter formato =
-                DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    // Construtor COM usuário (ação vinculada a alguém)
+    public Log(String mensagem, String tipo, Integer fkUsuario) {
+        this.mensagem = mensagem;
+        this.tipo = tipo;
+        this.dataHora = LocalDateTime.now();
+        this.fkUsuario = fkUsuario;
+    }
 
-        return "[" + dataHora.format(formato) + "] " + "[" + tipo + "] " + mensagem;
+    public Log() {
+    }
+
+    public String formatarLog() {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return "[" + dataHora.format(formato) + "] [" + tipo + "] " + mensagem;
+    }
+
+    public Integer getIdLog() {
+        return idLog;
+    }
+
+    public void setIdLog(Integer idLog) {
+        this.idLog = idLog;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Integer getFkUsuario() {
+        return fkUsuario;
+    }
+
+    public void setFkUsuario(Integer fkUsuario) {
+        this.fkUsuario = fkUsuario;
     }
 }
